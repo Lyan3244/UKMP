a0a92fba4ccd6f9364edc54d125b9547f6cad257
Lyan3244 <luthfannisapramunegari.xiao@gmail.com>
2023-03-22 01:36:52 +0700

    crud sprint 3 done

--- resources/views/index_order.blade.php
+++ resources/views/index_order.blade.php
@@ -1,38 +1,66 @@
+@extends('layouts.navbar')
+@section('content')
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Order</title>
 </head>
 <body>
+<div class="container mt-7 mx-auto md:px-12">
+    <!-- awal header bg abu-abu -->
+    <div class="mt-10 text-2xl text-gray-500 font-semibold whitespace-nowrap p-2">
+        Daftar Pesanan
+    </div>
+    <hr>
+    <div class="px-2 pt-2">
+        <a href="/penulis.ukm.um/dashboard" class="text-blue-700 hover:text-gray-500"> Dashboard</a> <span class="text-gray-500">> Daftar Pesanan</span>
+    </div>
+
+    <div class="pt-5">
     @foreach ($orders as $order)
-        <p>ID : {{$order->id}}</p>
-        <p>User : {{$order->user->name}}</p>
-        <p>{{$order->created_at}}</p>
-        @foreach($order->transactions as $transaction)
-        <p>Nama Produk yang dibeli : {{$transaction->product->name}}</p>
-        <p>Banyak Produk yang dibeli : {{$transaction->amount}}</p>
+    <div class="mx-auto border rounded-md w-4/6">
+        <div class="bg-gray-100 rounded-t">
+            <div class="px-5 py-3">
+            <p class="font-semibold text-lg">ID Pemesanan - {{$order->id}} oleh {{$order->user->name}}</p>
+            <p>{{$order->created_at}}</p>
+            </div>
+        </div>
+
+        <div class="px-14 py-3">
+        <p class="py-1">Detail Pemesanan Buku :</p>
+    @foreach($order->transactions as $transaction)
+        <p class="py-1">Judul Buku yang dibeli : {{$transaction->product->name}}</p>
+        <p class="py-1">Banyak Buku yang dibeli : {{$transaction->amount}} pcs</p>
         @php
             $total_price = $transaction->product->price * $transaction->amount;
         @endphp
-        <p>Total Pembayaran  = Rp{{$total_price}}</p>
+        <p class="py-1">Total Pembayaran  : Rp{{$total_price}}</p>
+        <hr>
     @endforeach
-        <p>
+        <p class="py-1 font-semibold"> Status Pembayaran :
             @if($order->is_paid == true)
-                paid
-            @else
-                unpaid
+            <span class="text-blue-700 font-semibold">Sudah Terbayar</span>    
+            @else 
+            <span class="text-red-500 font-semibold">Belum Terbayar</span>
                 @if($order->payment_receipt)
-                    <a href="{{url('storage/' . $order->payment_receipt)}}">show payment receipt</a>
+                    <a href="{{url('storage/' . $order->payment_receipt)}}" class="text-blue-700 hover:text-blue-500">Lihat Bukti Pembayaran</a>
                     <form action="{{route('confirm_payment', $order)}}" method="post">
                     @csrf
-                    <button type="submit">Confirm</button>
+                    <div class="pb-10">
+                    <button type="submit" class="bg-blue-700 px-4 py-2 rounded text-white hover:bg-blue-500 float-right">Konfirmasi</button>
+                    </div>
                     </form>
                 @endif
             @endif
         </p>
+        </div>
+    </div>
+    <br>
     @endforeach
+    </div>
+</div>
 </body>
 </html>
+@endsection
--- resources/views/show_order.blade.php
+++ resources/views/show_order.blade.php
@@ -1,3 +1,5 @@
+@extends('layouts.navbar')
+@section('content')
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,36 +9,72 @@
     <title>Show Order</title>
 </head>
 <body>
+<div class="container mt-7 mx-auto md:px-12">
+    <!-- awal header bg abu-abu -->
+    <div class="mt-10 text-2xl text-gray-500 font-semibold whitespace-nowrap p-2">
+        Checkout
+    </div>
+    <hr>
+    <div class="px-2 pt-2">
+        <a href="/cart" class="text-blue-700 hover:text-gray-500"> Tambah Keranjang</a> <span class="text-gray-500">>  Checkout</span>
+    </div>
     @php
         $total_price = 0;
     @endphp
-    <p>Order ID {{$order->id}}</p>
-    <p>By {{$order->user->name}}</p>
-    <p>Pengiriman ke {{$order->user->alamat}} <a href="/alamat" class="text-blue-700 pl-5">ubah</a></p>
 
-    @if($order->is_paid == true)
-        <p>Paid</p>
-    @else
-        <p>unpaid</p>
-    @endif
-    <hr>
-    @foreach($order->transactions as $transaction)
-        <p>{{$transaction->product->name}} - {{$transaction->amount}} pcs</p>
-        @php
-            $total_price += $transaction->product->price * $transaction->amount;
-        @endphp
-    @endforeach
-    <hr>
-        <p>Total : Rp{{$total_price}}</p>
-    <hr>
+    <div class="pt-5">
+        <div class="mx-auto border rounded-md w-4/6 py-3 px-7">
+            <p class="text-lg">Pesanan Oleh - {{$order->user->name}}</p>
+            <p class="text-lg">Pengiriman ke {{$order->user->alamat}} <a href="/alamat" class="text-blue-700 pl-5 hover:text-blue-500">ubah</a></p>
 
-    @if($order->is_paid == false && $order->payment_receipt == null)
-        <form action="{{route('submit_payment_receipt', $order)}}" method="post" enctype = "multipart/form-data">
-            @csrf
-            <label for="payment_receipt">Upload Your Payment Receipt</label><br>
-            <input type="file" name="payment_receipt" id="payment_receipt"><br>
-            <button type="submit">Submit Payment</button>
-        </form>
-    @endif
+            @if($order->is_paid == true)
+                <p class="font-semibold text-blue-700 text-lg">Sudah Terbayar</p>
+            @else
+                <p class="font-semibold text-red-500 text-lg">Belum Terbayar</p>
+            @endif
+        </div>
+    </div>
+
+    <div class="pt-5">
+        <div class="mx-auto border rounded-md w-4/6">
+        <table class="border-separate border-spacing-x-28 border-spacing-y-3">
+                <tr>
+                    <th>Buku yang Dipesan</th>
+                    <th>Harga Satuan</th>
+                    <th>Jumlah</th>
+                </tr>
+            @foreach($order->transactions as $transaction)
+                <tr>
+                    <td>{{$transaction->product->name}}</td>
+                    <td>Rp{{$transaction->product->price}}</td>
+                    <td>{{$transaction->amount}} pcs</td>
+                </tr>
+                @php
+                $total_price += $transaction->product->price * $transaction->amount;
+                @endphp
+            @endforeach
+                <tr>
+                    <td class="font-semibold">Total Pesanan</td>
+                    <td class="font-semibold text-lg text-blue-700">Rp{{$total_price}}</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+
+    <div class="pt-5">
+        <div class="mx-auto border rounded-md w-4/6 py-3 px-7">
+            @if($order->is_paid == false && $order->payment_receipt == null)
+                <form action="{{route('submit_payment_receipt', $order)}}" method="post" enctype = "multipart/form-data">
+                    @csrf
+                    <label for="payment_receipt" class="text-lg">Unggah Bukti Pembayaran</label>
+                    <div class="pt-3">
+                    <input type="file" name="payment_receipt" id="payment_receipt" class="border rounded-md"> <span><button type="submit" class="bg-blue-700 px-4 py-2 rounded text-white hover:bg-blue-500">Kirim</button></span>
+                    </div>
+                </form>
+            @endif
+        </div>
+    </div>
+</div>
 </body>
 </html>
+@endsection
