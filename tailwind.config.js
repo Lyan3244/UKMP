842a8ec4abaa558086895aca32485406e13c5731
Lyan3244 <luthfannisapramunegari.xiao@gmail.com>
2023-07-08 10:14:34 +0700

    8 Juli 2023

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ module.exports = {
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
-    "./node_modules/flowbite/**/*.js"
-    "./resources/**/*.css",
+    "./node_modules/flowbite/**/*.js",
+    "./resources/**/*.css"
   ],
   theme: {
     extend: {
